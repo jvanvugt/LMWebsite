0e0802a5c600c7c97d5b3e1984334c17166811ed
Marein Könings <marein@science.ru.nl>
2015-02-03 22:37:57 +0100

    angularfire's first steps!

--- app/application.js
+++ app/application.js
@@ -20,16 +20,21 @@ angular.module("navbarapp", ["controllers"])
   }});
 ;
 
-var controllers = angular.module("controllers", []);
+var controllers = angular.module("controllers", ["firebase"]);
 
-controllers.controller("OverzichtCtrl", function HomePageCtrlHomePageCtrl($scope) {
+controllers.controller("OverzichtCtrl", function OverzichtCtrl($scope, $firebase) {
   $scope.pageName = "Overzicht";
+  
+  var ref = new Firebase("https://cognac-monopoly.firebaseio.com/teams");
+  var sync = $firebase(ref);
+  // create a synchronized array for use in our HTML code
+  $scope.teams = sync.$asArray();
 });
 
-controllers.controller("TeamCtrl", function SecondPageCtrl($scope) {
+controllers.controller("TeamCtrl", function TeamCtrl($scope) {
   $scope.pageName = "Team";
 });
 
-controllers.controller("AdminCtrl", function ThirdPageCtrl($scope) {
+controllers.controller("AdminCtrl", function AdminCtrl($scope) {
   $scope.pageName = "Admin";
 });
