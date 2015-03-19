ea0df1d48dfa7e1301f18201b89f7b75b743888e
Marein Konings <marein@science.ru.nl>
2015-03-19 16:49:32 +0100

    admin page: add/remove users and teams

--- app/controllers.js
+++ app/controllers.js
@@ -31,11 +31,57 @@ monopolyControllers.controller('AdminCtrl', function AdminCtrl($scope, $firebase
   $scope.pageName = 'Admin';
   $scope.pageDesc = 'Voer admin functies uit';
 
-  var sync = $firebase(new Firebase(FIREBASE_URL+'teams'));
-  $scope.teams = sync.$asArray();
+  var teamsync = $firebase(new Firebase(FIREBASE_URL+'teams'));
+  $scope.teams = teamsync.$asArray();
+  
+  var usersync = $firebase(new Firebase(FIREBASE_URL+'users'));
+  $scope.users = usersync.$asArray();
+
+  $scope.judgeFilter = function (user) {
+    return user.roles && user.roles.judge;
+  };
+
+  $scope.eligibleMemberFilter = function (user ) {
+    
+    return !$scope.judgeFilter(user) && !user.team;
+  };
 
   $scope.addUser = function(user) {
-    var sync = $firebase(new Firebase(FIREBASE_URL+'users'));
-    sync.$push(user);
+    var sync = new Firebase(FIREBASE_URL+'users');
+    sync.push(user);
   };
+  
+  $scope.deleteUser = function(userId) {
+   var user = new Firebase(FIREBASE_URL+'users/'+userId);
+   user.child('team').once('value', function(snap) {
+     new Firebase(FIREBASE_URL+'teams/'+snap.val()+'/members/'+userId).remove();
+     user.remove();
+   });
+  };
+
+  $scope.addTeam = function(team, teamMembers) {
+    var sync = $firebase(new Firebase(FIREBASE_URL+'teams')).$asArray();
+    team.active = true;
+    team.balance = 0;
+    sync.$add(team).then(function(r){
+     members = new Firebase(FIREBASE_URL+'teams/'+r.key()+'/members');
+      teamMembers.forEach(function(userId) {
+        members.child(userId).set(true);
+        user = new Firebase(FIREBASE_URL+'users/'+userId);
+        user.child('team').set(r.key());
+      });
+   });
+  };
+
+  $scope.deleteTeam = function(teamId) {
+    var teamMembers = new Firebase(FIREBASE_URL+'teams/'+teamId+'/members');
+    teamMembers.on('child_added', function(snap) {
+      new Firebase(FIREBASE_URL+'users/'+snap.key()+'/team').remove();
+      snap.ref().remove();
+      teamMembers.on('value', function(snap) {
+        if (snap.numChildren() == 0)
+          teamMembers.parent().remove();
+      });
+    });
+  }; 
 });
